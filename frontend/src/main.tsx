0d7f08204c5a6dfaf5c5c674857331d779531b8e
0401chen <c932022014@gmail.com>
2026-07-29 14:38:52 +0800

    feat: activate Noname assistant branding

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import { installBranding } from "./branding";
 import "./styles.css";
 import "./reviewer.css";
 import "./demo.css";
+
+installBranding();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
