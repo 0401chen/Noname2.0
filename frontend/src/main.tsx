0df46f75030a9974b64f1f86d955cf44a5153745
0401chen <c932022014@gmail.com>
2026-07-29 13:11:08 +0800

    style: load guided demo styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./reviewer.css";
+import "./demo.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
